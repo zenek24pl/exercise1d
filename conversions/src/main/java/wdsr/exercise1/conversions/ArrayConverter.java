c9b2b78c307b7084f3190cd9c8987d1f60ad3584
Marek Strejczek <marek.strejczek@gmail.com>
2016-02-22 23:35:51 +0100

    Added javadoc for ArrayConverter

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	/**
+	 * Converts an array of strings to an array of integers.
+	 * @param strings to be converted to integers.
+	 * @return array of integers corresponding to the provided strings. Never null.
+	 * @throws NullPointerException if the argument is null.
+	 * @throws NumberFormatException if any of the strings does not represent an integer value.
+	 */
     public int[] convertToInts(String[] strings) {
         return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
     }
